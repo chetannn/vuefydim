60cd02029690e992d36fe769f8b4837c2f0fb995
chetan <chetankharel7@gmail.com>
2020-05-12 20:39:39 +0545

    settings form updated

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,81 +1,80 @@
 <template>
-   <v-app-bar flat dense color="primary" dark app>
-      <v-app-bar-nav-icon  @click="$emit('toggleDrawer')"></v-app-bar-nav-icon>
-      <v-toolbar-title class="white--text">{{navTitle}}</v-toolbar-title>
-      <v-spacer></v-spacer>
+  <v-app-bar flat dense color="primary" dark app>
+    <v-app-bar-nav-icon @click="$emit('toggleDrawer')"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">{{navTitle}}</v-toolbar-title>
+    <v-spacer></v-spacer>
 
-      <v-btn v-if="!loggedIn" text to="/register" rounded class="mr-4">Register</v-btn>
-      <v-btn v-if="!loggedIn" text rounded to="/login" class="mr-4">Login</v-btn>
+    <v-btn v-if="!loggedIn" text to="/register" rounded class="mr-4">Register</v-btn>
+    <v-btn v-if="!loggedIn" text rounded to="/login" class="mr-4">Login</v-btn>
 
-      <v-btn  v-if="loggedIn" text icon rounded @click="$emit('toggleTheme')" class="mr-4">
-        <v-icon>mdi-theme-light-dark</v-icon>
-      </v-btn>
+    <v-btn v-if="loggedIn" text icon rounded @click="$emit('toggleTheme')" class="mr-4">
+      <v-icon>mdi-theme-light-dark</v-icon>
+    </v-btn>
 
-      <v-btn  v-if="loggedIn" text icon rounded>
-        <v-icon class="mr-4">mdi-bell-outline</v-icon>
-      </v-btn>
+    <v-btn v-if="loggedIn" text icon rounded>
+      <v-icon class="mr-4">mdi-bell-outline</v-icon>
+    </v-btn>
 
-      <v-menu v-if="loggedIn" class="mr-4" offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-avatar size="40">
-              <img src="https://i.pravatar.cc/40" alt="avatar" />
-            </v-avatar>
-          </v-btn>
-        </template>
+    <v-menu v-if="loggedIn" class="mr-4" offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-avatar size="40">
+            <img src="https://i.pravatar.cc/40" alt="avatar" />
+          </v-avatar>
+        </v-btn>
+      </template>
 
-        <v-list>
-          <v-list-item @click="$router.push({name: 'Profile'})">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Profile</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+      <v-list>
+        <v-list-item @click="$router.push({name: 'Profile'})">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-          <v-list-item @click="$router.push({ name: 'Settings'})">
-            <v-list-item-icon>
-              <v-icon>mdi-wrench</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Settings</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+        <v-list-item @click="$router.push({ name: 'Settings'})">
+          <v-list-item-icon>
+            <v-icon>mdi-wrench</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-          <v-list-item @click="logout">
-            <v-list-item-icon>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Logout</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+        <v-list-item @click="logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
-      <!-- -->
-    </v-app-bar>
+    <!-- -->
+  </v-app-bar>
 </template>
 
 <script>
-import { authComputed  } from '@/store/helpers'
+import { authComputed } from '@/store/helpers';
 
 export default {
-    name: 'Navbar',
-    props: ['navTitle'],
-    computed: {
-      ...authComputed
-    },
-    methods: {
-      logout() {
-        console.log('logout')
-        this.$store.dispatch('logout')
-      }
+  name: 'Navbar',
+  props: ['navTitle'],
+  computed: {
+    ...authComputed
+  },
+  methods: {
+    logout() {
+      console.log('logout');
+      this.$store.dispatch('logout');
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,46 @@
 <template>
-  <h1>Settings Page</h1>
+  <div>
+    <h1>Settings Page</h1>
+    <v-card class="mx-auto" max-width="600">
+      <v-card-title>
+        <h3>Application Settings</h3>
+      </v-card-title>
+      <v-card-subtitle>Change Application Settings</v-card-subtitle>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-form>
+          <v-text-field outlined label="App Title"></v-text-field>
+          <v-text-field outlined label="App Subtitle"></v-text-field>
+          <v-label>Choose Color Theme</v-label>
+          <v-color-picker v-model="color" class="mb-4" show-swatches></v-color-picker>
+        </v-form>
+      </v-card-text>
+
+      <v-divider />
+      <v-card-actions>
+        <v-btn color="success">Save</v-btn>
+        <v-btn color="info">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      color: ''
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log('root', this.$root);
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.v-avatar {
+  cursor: pointer;
+}
 </style>
